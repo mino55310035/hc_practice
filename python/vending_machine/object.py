7e23993676b545b5c8d29a5f4e0d59b8f36a3d0a
mino55310035 <kgtgys53@gmail.com>
2025-08-21 00:52:57 +0900

    vending_machineを修正3

--- python/vending_machine/object.py
+++ python/vending_machine/object.py
@@ -1,6 +1,3 @@
-from curses import ALL_MOUSE_EVENTS
-
-
 class Suica:
     def __init__(self, balance=500):
         self._balance = balance
@@ -29,10 +26,9 @@ class Suica:
 
 
 class Juice:
-    def __init__(self, name, price, stock):
+    def __init__(self, name, price):
         self._name = name
         self._price = price
-        self._stock = stock
 
     @property
     def name(self):
@@ -42,61 +38,54 @@ class Juice:
     def price(self):
         return self._price
 
-    @property
-    def stock(self):
-        return self._stock
-
-
 class Vending_machine:
     def __init__(self):
-        self.juice_list = [
-            Juice("ペプシ", 150),
-            Juice("いろはす", 120),
-            Juice("モンスター", 350)
-        ]
-
         self._stock = []
-        for _ in range(5):
-            self._stock.append(Juice("ペプシ", 150))
-        for _ in range(5):
-            self._stock.append(Juice("いろはす", 120))
-        for _ in range(5):
-            self._stock.append(Juice("モンスター", 350))
         
-        
+        items = [
+            ("ペプシ", 150),
+            ("いろはす", 120),
+            ("モンスター", 230)
+        ]
+        for name, price in items:
+            for _ in range(5):
+                self._stock.append(Juice(name, price))
+
         self._sales = 0
 
-    def purchase(self, suica, item_index):
-        if not self.can_buy(suica, item_index):
+    def purchase(self, suica, item_name):
+        if not self.can_buy(suica, item_name):
             raise Exception("購入できません")
-        juice = self.juice_list[item_index]
-        self._stock.remove(juice)
-        self._sales += juice.price
-        suica.pay(juice.price)
-
+        for juice in self._stock:
+            if juice.name == item_name:
+                self._stock.remove(juice)
+                self._sales += juice.price
+                suica.pay(juice.price)
+                break
     @property
     def sales(self):
         return self._sales
 
-    def can_buy(self, suica, item_index):
-        juice = self.juice_list[item_index]
-        for j in self._stock:
-            if j.name == juice.name and suica._balance > juice.price:
+    def can_buy(self, suica, item_name):
+        for juice in self._stock:
+            if juice.name == item_name and suica._balance >= juice.price:
                 return True
         else:
             return False
 
     def can_buy_list(self, suica):
-        item_list = []
-        for i in range(len(self.juice_list)):
-            if self.can_buy(suica, i):
-                item_list.append(self.juice_list[i].name)
-        return item_list
+        return list({juice.name for juice in self._stock if self.can_buy(suica, juice.name)})
 
-    def restock(self, item_index, amount):
-        juice = self.juice_list[item_index]
+    def restock(self, item_name, amount):
+        price = None
+        for juice in self._stock:
+            if juice.name == item_name:
+                price = juice.price
+                break
+        if price is None:
+            raise ValueError(f"{item_name}は存在しません")
         for _ in range(amount):
-            self._stock.append(Juice(juice.name, juice.price))
+            self._stock.append(Juice(item_name, price))
 
 
         
