eb465a2c394d15fbc2741065aed1fa5135711555
mino55310035 <kgtgys53@gmail.com>
2025-08-26 11:37:59 +0900

    calendar.jsを作成

---
+++ javascript/calendar.js
@@ -0,0 +1,57 @@
+console.log(process.argv);
+
+const today = new Date;
+
+let year, month;
+if (process.argv.length === 2){
+    year = today.getFullYear();
+    month = today.getMonth() + 1;
+} else if (process.argv.length === 4 && process.argv[2] === "-m") {
+    year = today.getFullYear();
+    const inputMonth = process.argv[3];
+    const monthNum = parseInt(inputMonth);
+    if (isNaN(monthNum)){
+        console.log("数字で入力してください");
+        process.exit(1);
+    } else if (monthNum < 1 || monthNum > 12){
+        console.log("1~12の数字を入力してください");
+        process.exit(1);
+    } 
+    month = monthNum;
+}
+
+const header = `日 月 火 水 木 金 土`;
+
+const dateInMonth = new Date(year, month, 0).getDate();
+
+const firstDay = new Date(year, month -1, 1);
+const startDay = firstDay.getDay();
+
+const calendar = [];
+ 
+for (let i=0; i < 42; i++) {
+    if (i < startDay){
+        calendar[i] = " ".padStart(2, " ");
+    }else if(i < startDay + dateInMonth){
+        const currentDay = i - startDay + 1;
+        calendar[i] = String(currentDay).padStart(2, " ");
+    } else {
+        calendar[i] = " ".padStart(2, " ");
+    }
+ }
+
+ let calendarText = " ";
+ for (let i = 0; i < calendar.length; i++) {
+    calendarText += calendar[i];
+    if((i + 1) % 7 === 0){
+        calendarText += "\n";
+    } else{
+        calendarText += " ";
+    }
+ }
+
+ const calendarDisplay = `    ${year}年${month}月
+${header}
+${calendarText}`;
+
+console.log(calendarDisplay);
